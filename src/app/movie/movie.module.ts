f0141bac695548a05b8eb507db10c058f4c2ce76
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-22 11:41:35 -0500

    Movie resolver added, also imported core module to movies module

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MovieRoutingModule } from './movie-routing.module';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { CoreModule } from '../core/core.module';
+import { MovieResolver } from './movie-detail/movie-detail-resolver.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    MovieRoutingModule
+    MovieRoutingModule,
+    CoreModule
+  ],
+  providers: [
+    MovieResolver
   ],
   declarations: [MovieDetailComponent]
 })
