204b625b89675be090ec5df28bc98f238ac73b75
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-22 10:19:46 -0500

    Mocks sistem eliminated,temporaly (cosume the movie database for now)

--- src/app/mokcs/movies.mock.ts
+++
@@ -1,3 +0,0 @@
-import { Movie } from '../core/models/v3/';
-import * as movies_json from './json/movies.json';
-export const MOVIES: Movie[] = (<Movie[]> movies_json);
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,7 +4,3 @@ interface NodeModule {
   id: string;
 }
 
-declare module "*.json" {
-    const value: any;
-    export default value;
-}
