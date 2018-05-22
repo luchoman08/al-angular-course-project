612267981d3ee71e6e02818e92dff6b74b5e30e5
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-22 12:50:01 -0500

    Movie detail component now support videos in movie

--- src/app/movie/movie-detail/movie-detail.component.ts
+++ src/app/movie/movie-detail/movie-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../core/models/v3/movie.model';
+import { MovieService } from '../../core/services/v3/movie.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-detail',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-detail.component.scss']
 })
 export class MovieDetailComponent implements OnInit {
+  movie: Movie;
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService,
+    private router: Router,
 
-  constructor() { }
+  ) {
+    this.movie = new Movie();
+  }
 
   ngOnInit() {
+    this.route.data.subscribe(
+      (data: { movie: Movie }) => {
+        this.movie = data.movie;
+        console.log(this.movie.videos);
+      }
+    );
   }
 
 }
