67ad90112c5d2ea3c218ac097e7efebc380f6a93
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-22 11:44:19 -0500

    Fix error: url from the movie db api is not found, removed end slash from base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiTMDBv3: 'https://api.themoviedb.org/3/',
-  apiTMDBv4: 'https://api.themoviedb.org/4/'
+  apiTMDBv3: 'https://api.themoviedb.org/3',
+  apiTMDBv4: 'https://api.themoviedb.org/4'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiTMDBURL_V3: 'https://api.themoviedb.org/3/',
-  apiTMDB_URL_V4: 'https://api.themoviedb.org/4/',
+  apiTMDBURL_V3: 'https://api.themoviedb.org/3',
+  apiTMDB_URL_V4: 'https://api.themoviedb.org/4',
   apiTMDB_Key_V3: '1d78ec525ceef86a4b4a46a74e137e37'
 };
