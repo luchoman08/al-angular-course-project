c62cd537c9a2ff1a957002cf77549601dd4e8e0e
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-22 11:19:26 -0500

    Api interceptor added

---
+++ src/app/core/interceptors/api-key.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../environments/environment';
+
+@Injectable()
+export class ApiKeyInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const headersConfig = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+    const paramsConfig = {
+        'api_key': environment.apiTMDB_Key_V3
+    };
+    const request = req.clone({ setParams: paramsConfig });
+    return next.handle(request);
+  }
+}
