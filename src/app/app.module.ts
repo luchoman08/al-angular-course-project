671ac0b2c42fbdc1dfe4e9d537a7b92ddfcbab8f
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-22 20:39:31 -0500

    Some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
+    LoadingBarModule.forRoot(),
     SharedModule,
     MaterialModule
   ],
