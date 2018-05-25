55f15fc672191ef380595b13dc8dabdc38ac3aaf
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-25 17:07:29 -0500

    Images url added to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,6 @@ export const environment = {
   production: false,
   apiTMDBURL_V3: 'https://api.themoviedb.org/3',
   apiTMDB_URL_V4: 'https://api.themoviedb.org/4',
-  apiTMDB_Key_V3: '1d78ec525ceef86a4b4a46a74e137e37'
+  apiTMDB_Key_V3: '1d78ec525ceef86a4b4a46a74e137e37',
+  imagesTMDB_URL: 'https://image.tmdb.org/t/p'
 };
