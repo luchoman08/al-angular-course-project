ee16a4ab22cf1f85c0a789cad1a1eea0f8adfae9
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-25 17:07:40 -0500

    apiImageService added

--- src/app/core/services/shared/index.ts
+++ src/app/core/services/shared/index.ts
@@ -1,2 +1,3 @@
 export * from './api.service';
 export * from './shared-functions';
+export * from './apiImages.service';
--- src/app/core/services/v3/image.service.ts
+++ src/app/core/services/v3/image.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Person } from '../../models/v3';
-import { ApiService } from '../shared/api.service';
-import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operators/map';
-import { paramsAppendToResponse } from '../shared/shared-functions';
+import { ApiImagesService } from '../shared/apiImages.service';
 @Injectable()
 export class ImageService {
   constructor (
+    private apiImageService: ApiImagesService
   ) {}
 
-  getBySize(path: string,
+  get(path: string,
     size: string
     ): Observable<Person> {
-        const params = paramsAppendToResponse(videos, images);
-        return this.apiService.get('/person/' + id, params)
+        return this.apiImageService.get(`${size}/${path}`)
         .pipe(map(data => data));
   }
-
- 
 }
