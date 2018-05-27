7ed0aedbf9189045c33c49439123d25834f7ac82
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-26 22:03:07 -0500

    Image support enum and abstract class created

---
+++ src/app/core/models/abstract/image.abstract.ts
@@ -0,0 +1,13 @@
+import { ImageSize } from "../enums/image-size.enum";
+
+export abstract class Image {
+    aspect_ratio: number;
+    file_path: string;
+    height: number;
+    iso_639_1: string;
+    vote_average: number;
+    vote_count: number;
+    width: number;
+    sizes: {};
+    constructor() { }
+}
