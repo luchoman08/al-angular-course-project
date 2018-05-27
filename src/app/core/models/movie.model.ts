d7dd2520578d8c71ae2f538a80bb531714e8720b
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-27 14:41:20 -0500

    Json to typescript support

--- src/app/core/models/movie.model.ts
+++ src/app/core/models/movie.model.ts
@@ -1,5 +1,28 @@
-import { Genre, Video,  SpokenLanguage, ProductionCompany } from '@app/core/models/';
-import { Image } from '@app/core/images';
+import { Genre, Video,  SpokenLanguage, ProductionCompany } from './';
+import { Image } from '../images';
+
+export interface MovieJSON {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    vote_average: number;
+    vote_count: number;
+    imdb_id: string | null;
+    backdrop_path: string;
+    genre_ids: number[];
+    original_language?: string;
+    original_title: string;
+    popularity: number; // between 0 and 10
+    production_companies: ProductionCompany[];
+    spoken_languages: SpokenLanguage[];
+    video: boolean; // movie video preview available?
+    video_count: number;
+    genres?: Genre[];
+    videos?: {results: Video[]};
+    images?: { backdrops: Image[], posters: Image[]};
+}
+
 export class Movie {
     id: number;
     title: string;
@@ -20,4 +43,17 @@ export class Movie {
     genres?: Genre[];
     videos?: {results: Video[]};
     images?: { backdrops: Image[], posters: Image[]};
+    public static fromJSON(json: MovieJSON): Movie {
+      const movie = Object.create(Movie.prototype);
+      return Object.assign(movie, json);
+      }
+    getVideoKeys(): string[] {
+      if (this.videos) {
+        console.log('una vez y otra mas');
+        return this.videos.results.map((video: Video) => video.key );
+      } else {
+        return new Array<string>();
+      }
+    }
+
 }
