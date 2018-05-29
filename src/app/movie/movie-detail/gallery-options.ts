f9f2b13e42cba6cc31bdfca31aaa85f23e8806a4
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-29 00:23:20 -0500

    Galery options now only for full screen visualization

--- src/app/movie/movie-detail/gallery-options.ts
+++ src/app/movie/movie-detail/gallery-options.ts
@@ -1,25 +1,8 @@
 import { NgxGalleryAnimation } from 'ngx-gallery';
 
-export const galleryOptions = [
-  {
-      width: '600px',
-      height: '400px',
-      thumbnailsColumns: 4,
-      imageAnimation: NgxGalleryAnimation.Slide
-  },
-  // max-width 800
-  {
-      breakpoint: 800,
-      width: '100%',
-      height: '600px',
-      imagePercent: 80,
-      thumbnailsPercent: 20,
-      thumbnailsMargin: 20,
-      thumbnailMargin: 20
-  },
-  // max-width 400
-  {
-      breakpoint: 400,
-      preview: true
-  }
-];
+export const galleryOptions = [{
+  image: false,
+  thumbnails: false,
+  width: '0px',
+  height: '0px'
+}];
