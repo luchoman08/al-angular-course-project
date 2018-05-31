a1fb5f5d794b1a3dfcf7875f4a0128785d5e6022
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-31 12:15:12 -0500

    Search movies at toolbar and make toolbar black

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material';
+import { CoreModule } from '@app/core';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
+    CoreModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
     AppRoutingModule,
     LoadingBarModule.forRoot(),
     SharedModule,
