e55814a92883628bbc8a53a9a266258310496406
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-01 17:09:15 -0500

    Image size problems with posibles values solved

--- src/app/core/images/enums/profile-image-interface.enum.ts
+++ src/app/core/images/enums/profile-image-interface.enum.ts
@@ -1,14 +1,14 @@
 import { ImageSize } from './image-size.enum';
 export interface ProfileImageSizesInterface {
-  W92: string;
-  W154: string;
+  W45: string;
+  W185: string;
   H632: string;
   ORIGINAL: string;
 }
 
 export const ProfileImageSizes =  {
-    W92: ImageSize.W45,
-    W154: ImageSize.W185,
+    W45: ImageSize.W45,
+    W185: ImageSize.W185,
     H632: ImageSize.H632,
     ORIGINAL: ImageSize.ORIGINAL
 } as ProfileImageSizesInterface;
