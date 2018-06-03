59a58e8c8054ea08c241db1218878f8461cdca60
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-02 20:10:22 -0500

    Non necesary imports elimitade from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatSlideToggleModule, MatIconModule, MatToolbarModule, MatChipsModule, MatInputModule, MatSidenavModule, MatButtonModule } from '@angular/material';
+
+import {
+  MatSlideToggleModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatChipsModule,
+  MatInputModule,
+  MatSidenavModule,
+  MatButtonModule
+} from '@angular/material';
+
 import { CoreModule } from '@app/core';
 
 @NgModule({
