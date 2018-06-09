0d99f3c3ab236d50df9d30cba438a8d0dadd1df8
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-09 14:31:22 -0500

    Tv show detail controller and view updated from nothing to basic functionality based on movies detail

--- src/app/tvshow/tvshow-detail/tvshow-detail.component.ts
+++ src/app/tvshow/tvshow-detail/tvshow-detail.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import {
+  NgxGalleryComponent,
+  NgxGalleryOptions,
+  NgxGalleryImage
+} from 'ngx-gallery';
+
+import { TVShow, PosterImageSizesInterface, GalleryImagesService, POSTER_IMAGE_SIZES,
+  galleryOptionsFullScreenOnly } from '@app/core';
+import { ImageURLPipe, YoutubeVideoDialogComponent } from '@app/shared';
 
 @Component({
   selector: 'app-tvshow-detail',
@@ -7,9 +19,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TvshowDetailComponent implements OnInit {
 
-  constructor() { }
+  tvShow: TVShow;
+  @ViewChild('onlyPreviewGallery') onlyPreviewGallery: NgxGalleryComponent;
+  POSTER_IMAGE_SIZES: PosterImageSizesInterface;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+  constructor(
+    private route: ActivatedRoute,
+    public dialog: MatDialog,
+    private galleryImageService: GalleryImagesService,
+    private imgURLPipe: ImageURLPipe
+  ) {
+    this.POSTER_IMAGE_SIZES =  POSTER_IMAGE_SIZES;
+    this.tvShow = new TVShow();
+    this.galleryImages = new Array<NgxGalleryImage>();
+  }
+  openPreviewImages(): void {
+    this.onlyPreviewGallery.openPreview(0);
+  }
+  openTrailers(): void {
+    console.log(this.tvShow.getVideoKeys());
+    const dialogRef = this.dialog.open(YoutubeVideoDialogComponent, {
+      width: '750px',
+      height: '500px',
+      data: {
+        video_keys: this.tvShow.getVideoKeys()
+      }
+    });
+  }
 
   ngOnInit() {
+    this.galleryOptions = galleryOptionsFullScreenOnly;
+    this.route.data.subscribe((data: { tvShow: TVShow }) => {
+      this.tvShow = new TVShow();
+      this.tvShow = TVShow.fromJSON(data.tvShow);
+
+      this.galleryImages = this.galleryImageService.
+      getBackdropGalleryImages(this.tvShow.images.backdrops);
+
+  });
   }
 
 }
