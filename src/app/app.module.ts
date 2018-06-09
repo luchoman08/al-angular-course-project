d6ae991b610a7d05aaffc6df3864aebfcb687b36
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-09 11:45:55 -0500

    Browse module and browser animations module now are imoprted in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -18,6 +15,8 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CoreModule,
     SharedModule,
     AppRoutingModule,
