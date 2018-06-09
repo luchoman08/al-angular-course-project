27958ab127c8bacc7cd5726baf204cc502051bcd
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-09 14:31:50 -0500

    Tv show interface added and fromJSON creation of tvshow added too

--- src/app/core/models/tv-show.model.ts
+++ src/app/core/models/tv-show.model.ts
@@ -1,6 +1,23 @@
 import { Video, TvSeriesStatusEnum } from '@app/core/';
 import { TvSeason, Image } from '@app/core/';
 import { TvSeriesTypeEnum } from './enums/tvseries-type.enum';
+
+export interface TVShowInterface {
+    id: number;
+    name: string;
+    overview: string;
+    vote_average: number;
+    vote_count: number;
+    popularity: number; // 0 to 10 value
+    in_production: boolean;
+    status: TvSeriesStatusEnum ;
+    type: TvSeriesTypeEnum;
+    poster_path: string;
+    backdrop_path: string;
+    seasons: TvSeason[];
+    videos?: {results: Video[]};
+    images?: { backdrops: Image[], posters: Image[]};
+}
 export class TVShow {
     id: number;
     name: string;
@@ -16,4 +33,16 @@ export class TVShow {
     seasons: TvSeason[];
     videos?: {results: Video[]};
     images?: { backdrops: Image[], posters: Image[]};
+
+    public static fromJSON(json: TVShowInterface) {
+        const tvShow = Object.create(TVShow.prototype);
+        return Object.assign(tvShow, json);
+    }
+    getVideoKeys(): string[] {
+        if (this.videos) {
+          return this.videos.results.map((video: Video) => video.key );
+        } else {
+          return new Array<string>();
+        }
+      }
 }
