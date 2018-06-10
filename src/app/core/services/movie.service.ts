de3fa7f113c2c0c7d6d317f388c7d7ffe4c6dcab
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-09 21:50:42 -0500

    Movies service get popular and get in theatres

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -6,11 +6,24 @@ import { ApiService } from './shared';
 
 import { map } from 'rxjs/operators';
 import { paramsAppendToResponse } from '@app/core/services/shared';
+import { Results } from '../models/results.model';
+import { HttpParams } from '@angular/common/http';
 @Injectable()
 export class MovieService {
   constructor (
     private apiService: ApiService
   ) {}
+
+  private getResultsMultiplePage(url: string, page?: number): Observable<Results<Movie>>{
+    if (page) {
+      const params = new HttpParams().set('page', String(page));
+      return this.apiService.get(url, params)
+      .pipe( map ((data: Results<Movie>) => data));
+    } else {
+    return this.apiService.get(url)
+    .pipe( map ((data: Results<Movie>) => data));
+    }
+  }
 
   get(id,
       videos: Boolean = false,
@@ -21,6 +34,12 @@ export class MovieService {
         .pipe(map(data => data));
   }
 
+  getPopular(page?: number): Observable<Results<Movie>> {
+    return this.getResultsMultiplePage('/movie/popular', page);
+  }
+  getInTheatres(page?: number): Observable<Results<Movie>> {
+    return this.getResultsMultiplePage('/movie/now_playing', page);
+  }
 
 }
 
