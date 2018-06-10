046a2987647b62e84c90de04d70c45e368555aae
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-10 13:26:13 -0500

    Gallery images service suport for tv series

--- src/app/core/services/gallery-images.service.ts
+++ src/app/core/services/gallery-images.service.ts
@@ -31,11 +31,34 @@ export class GalleryImagesService {
 
       return galleryImages;
     }
-    getBackdropDescriptionGalleryImages(movies: Movie[]): NgxGalleryImage[] {
+    /**
+     * Get backdrop image description in gallery ngx for a movie
+     *
+     * @private
+     * @param {string} description
+     * @param {boolean} vote_average // include vote avg and start icon
+     * @param {boolean} vote_count // include text 'Vote count: <vote_count>
+     * @returns {string}
+     * @memberof GalleryImagesService
+     */
+    private getBackdropDescription( description: string, vote_average?: number, vote_count?: number): string {
+      let backdropDescription: string = description;
+      if (vote_average) {
+        backdropDescription = backdropDescription.concat('    ' , '&#9733;', String(vote_average));
+      }
+      if (vote_count) {
+        backdropDescription = backdropDescription.concat('    Votes: ', String(vote_count));
+      }
+      return backdropDescription;
+    }
+    getMovieBackdropDescriptionGalleryImages(movies: Movie[]): NgxGalleryImage[] {
       const galleryImages = new Array<NgxGalleryImage>();
         for (const movie of movies.sort(Movie.sortMethod)) {
           galleryImages.push({
-            description: movie.title + '    ' + '&#9733;' + movie.vote_average,
+            description: this.getBackdropDescription(
+              movie.title,
+              movie.vote_average,
+              movie.vote_count),
             // url: './movies/' + movie.id,
             small: this.imageURLPipe.transform(
               movie.backdrop_path,
