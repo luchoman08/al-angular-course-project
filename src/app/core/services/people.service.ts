4b45d786438b865c528be5fb3cb2b5137e56114b
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-11 10:14:50 -0500

    People make from json function added to people service

--- src/app/core/services/people.service.ts
+++ src/app/core/services/people.service.ts
@@ -4,13 +4,29 @@ import { ApiService, paramsAppendToResponse } from './shared';
 
 
 import { map } from 'rxjs/operators';
-import { PersonCreditsCombinedModel, Person } from '@app/core/models';
+import {
+  PersonCreditsCombinedModel,
+  Person,
+  PersonInterface
+} from '@app/core/models';
+
 @Injectable()
 export class PeopleService {
   constructor (
     private apiService: ApiService
   ) {}
-
+  /**
+   * Make a person model object from json with structure in person interface
+   *
+   * @private
+   * @param { PersonInterface } json
+   * @returns { Person } person
+   * @memberof PeopleService
+   */
+  public static makeFromJSON(json: PersonInterface): Person {
+    const tvShow = Object.create(Person.prototype);
+    return Object.assign(tvShow, json);
+  }
   get(id,
       videos: Boolean = false,
       images: Boolean = false
