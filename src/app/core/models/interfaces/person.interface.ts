98736638b36931526317a47aec78d605a2cd0adb
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-11 10:13:12 -0500

    Tv show interface moved to its own file , person interface created

---
+++ src/app/core/models/interfaces/person.interface.ts
@@ -0,0 +1,16 @@
+import { Gender, Image } from '../';
+export interface PersonInterface {
+    id: number;
+    name: string;
+    popularity: number;
+    also_known_as: string[];
+    profile_path: string; // code of image to profile image
+    biography: string;
+    images?: { profiles: Image[]};
+    birthday: string;
+    deathday: string;
+    gender: Gender;
+    imdb_id: string;
+    place_of_birth: string | null;
+    homepage: string | null;
+}
--- src/app/core/models/tv-show.model.ts
+++ src/app/core/models/tv-show.model.ts
@@ -1,24 +1,15 @@
-import { Video, TvSeriesStatusEnum } from '@app/core/';
-import { TvSeason, Image } from '@app/core/';
+import {
+  TvSeason,
+  Image,
+  TvSeriesStatusEnum,
+  Video
+} from './';
+
 import { TvSeriesTypeEnum } from './enums/tvseries-type.enum';
 
-export interface TVShowInterface {
-    id: number;
-    name: string;
-    overview: string;
-    vote_average: number;
-    vote_count: number;
-    popularity: number; // 0 to 10 value
-    in_production: boolean;
-    status: TvSeriesStatusEnum ;
-    type: TvSeriesTypeEnum;
-    poster_path: string;
-    backdrop_path: string;
-    seasons: TvSeason[];
-    videos?: {results: Video[]};
-    images?: { backdrops: Image[], posters: Image[]};
-}
-export class TVShow {
+import {TvShowInterface } from './interfaces';
+
+export class TVShow implements TvShowInterface {
     id: number;
     name: string;
     overview: string;
@@ -35,7 +26,7 @@ export class TVShow {
     videos?: {results: Video[]};
     images?: { backdrops: Image[], posters: Image[]};
 
-    public static fromJSON(json: TVShowInterface) {
+    public static fromJSON(json: TvShowInterface) {
         const tvShow = Object.create(TVShow.prototype);
         return Object.assign(tvShow, json);
     }
