730b86c98bbd1fe72e112e79f4d9d64470b65069
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-11 10:15:32 -0500

    Tv show service, make tv from json function and local imports instead @app..

--- src/app/core/services/tv-show.service.ts
+++ src/app/core/services/tv-show.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ApiService } from './shared/api.service';
-import { TVShow } from '@app/core/';
+
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { ApiService } from './shared/api.service';
 import { paramsAppendToResponse } from './shared/shared-functions';
+import { TVShow, TvShowInterface } from '@app/core/models';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TvShowService {
 
+  /**
+   * Get object with structure of tvshow interface and return new tvshow model constructed with the info from given object
+   *
+   * @static
+   * @param {TvShowInterface} json
+   * @returns {TVShow}
+   * @memberof TvShowService
+   */
+  public static makeFromJSON(json: TvShowInterface): TVShow {
+    const person = Object.create(TVShow.prototype);
+    return Object.assign(person, json);
+  }
   constructor(private apiService: ApiService) { }
   get(id,
     videos: Boolean = false,
