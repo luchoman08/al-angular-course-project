1f9f445d3cdbf42323c07a1e7058ea983193b3c2
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-11 10:17:21 -0500

    Support non defined images in imageURL pipe

--- src/app/shared/pipes/imagesURL.pipe.ts
+++ src/app/shared/pipes/imagesURL.pipe.ts
@@ -1,14 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { environment } from '@env/environment';
-import { ImageSizeValueModel } from '@app/core/';
+
+import {
+  ImageSizeValueModel,
+  MediaTypeEnum
+} from '@app/core/models';
+
 
 @Pipe({
   name: 'imageURL'
 })
 export class ImageURLPipe implements PipeTransform {
     api_url  = environment.imagesTMDB_URL;
-    transform(path: string, size: ImageSizeValueModel): string  {
-    return `${this.api_url}/${size.name}${path}`;
+    transform(path: string, size: ImageSizeValueModel, mediaType?: MediaTypeEnum): string  {
+      if ( path ) {
+        return `${this.api_url}/${size.name}${path}`;
+      } else if ( mediaType ) {
+        switch ( mediaType ) {
+          case MediaTypeEnum.MOVIE: {
+            return './assets/img/posters/poster_default.png';
+          }
+          case MediaTypeEnum.PERSON: {
+            return './assets/img/people/default-profile-men.png';
+          }
+        }
+      }
 
     }
   }
