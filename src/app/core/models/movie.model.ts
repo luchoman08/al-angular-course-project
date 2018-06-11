174fd1939baa8b4927b8d1fbaaf54b702be816b4
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-11 10:11:51 -0500

    Movie interface moved to its own file

--- src/app/core/models/movie.model.ts
+++ src/app/core/models/movie.model.ts
@@ -6,29 +6,9 @@ import {
   ProductionCompany
 } from './';
 
-export interface MovieInterface {
-    id: number;
-    title: string;
-    overview: string;
-    poster_path: string;
-    vote_average: number;
-    vote_count: number;
-    imdb_id: string | null;
-    backdrop_path: string;
-    genre_ids: number[];
-    original_language?: string;
-    original_title: string;
-    popularity: number; // between 0 and 10
-    production_companies: ProductionCompany[];
-    spoken_languages: SpokenLanguage[];
-    video: boolean; // movie video preview available?
-    video_count: number;
-    genres?: Genre[];
-    videos?: {results: Video[]};
-    images?: { backdrops: Image[], posters: Image[]};
-}
+import { MovieInterface } from './interfaces';
 
-export class Movie {
+export class Movie implements MovieInterface {
     id: number;
     title: string;
     overview: string;
