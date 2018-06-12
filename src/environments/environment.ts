4b8b9cfb8a52dbb0573185fdc84a53517e919e83
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-11 22:33:09 -0500

    Default language in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,6 @@ export const environment = {
   apiTMDBURL_V3: 'https://api.themoviedb.org/3',
   apiTMDB_URL_V4: 'https://api.themoviedb.org/4',
   apiTMDB_Key_V3: '1d78ec525ceef86a4b4a46a74e137e37',
-  imagesTMDB_URL: 'https://image.tmdb.org/t/p'
+  imagesTMDB_URL: 'https://image.tmdb.org/t/p',
+  language: 'en'
 };
