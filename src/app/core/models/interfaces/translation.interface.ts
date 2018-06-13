864203111557a27d9ed72d0aa486722847f40064
luchoman <luchoman08@gmail.com>
2018-06-13 17:58:07 -0500

    Translation interface added

---
+++ src/app/core/models/interfaces/translation.interface.ts
@@ -0,0 +1,7 @@
+export interface TranslationInterface {
+    iso_3166_1: string;
+    iso_639_1: string;
+    name: string;
+    english_name: string;
+    data: { title: string, overview: string, homepage: string};
+}
