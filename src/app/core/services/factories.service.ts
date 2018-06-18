46d84967bb9def3b5ebaecd60a1efab2615368cc
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-17 23:13:35 -0500

    Genres factory functions added to factory service

--- src/app/core/services/factories.service.ts
+++ src/app/core/services/factories.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { MovieFactory } from '@app/core/factories';
-import { MovieInterface, Movie } from '@app/core/models';
+import { MovieInterface, Movie, ResultsInterface, GenreInterface, Genre } from '@app/core/models';
+import { MovieResultsFactory } from '@app/core/factories/movie-results-factory';
+import { GenreFactory } from '@app/core/factories/genres-factory';
 /**
  * Provider of all factories for non directly dependence and instanciation of factories in each service
  * Also allow a common point of intersection of all factories, services does not depend directly of 
@@ -11,10 +13,23 @@ import { MovieInterface, Movie } from '@app/core/models';
 })
 export class FactoriesService {
   private movieFactory: MovieFactory;
+  private movieResultsFactory: MovieResultsFactory;
+  private genreFactory: GenreFactory;
   constructor() {
     this.movieFactory = new MovieFactory();
+    this.movieResultsFactory = new MovieResultsFactory();
+    this.genreFactory = new GenreFactory();
    }
    makeMovie( movieInterface: MovieInterface ): Movie {
      return this.movieFactory.make( movieInterface );
    }
+   makeMovieResults( movieInterfaceResults: ResultsInterface<MovieInterface>): ResultsInterface<Movie> {
+    return this.movieResultsFactory.make(movieInterfaceResults);
+   }
+   makeGenre( genreInterface: GenreInterface ): Genre {
+     return this.genreFactory.make(genreInterface);
+   }
+   makeGenres( genreInterfaces: GenreInterface[] ): Genre[] {
+    return this.genreFactory.makeMultiple(genreInterfaces);
+  }
 }
