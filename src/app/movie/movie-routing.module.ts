922222f4a3278a9c5e4f4f9c4ee18f95cb652847
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-19 11:21:07 -0500

    Movie routing module, using resolvers from app core and movies by keyword route added

--- src/app/movie/movie-routing.module.ts
+++ src/app/movie/movie-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
-import { MovieResolver } from './movie-detail/movie-detail-resolver.service';
+import { MovieResolver } from '@app/core/resolvers/movie-detail-resolver.service';
 import { MoviesHomeComponent } from './movies-home/movies-home.component';
 import { MovieFullCreditsComponent } from './movie-full-credits/movie-full-credits.component';
-import { MovieFullCreditsResolver } from '@app/movie/movie-full-credits/movie-full-credits-resolver.service';
+import { MovieFullCreditsResolver } from '@app/core/resolvers/movie-full-credits-resolver.service';
 import { MovieSearchComponent } from '@app/movie/movie-search/movie-search.component';
 import { MoviesPopularListComponent } from '@app/movie/movies-popular-list/movies-popular-list.component';
 import { MovieInTheatresListComponent } from '@app/movie/movie-in-theatres-list/movie-in-theatres-list.component';
 import { MovieTopRatedListComponent } from '@app/movie/movie-top-rated-list/movie-top-rated-list.component';
 import { MovieListByGenreComponent } from '@app/movie/movie-list-by-genre/movie-list-by-genre.component';
-import { MovieListByGenreResolver } from './movie-list-by-genre/movie-list-by-genre-resolver.service';
-import { GenreResolver } from '@app/movie/movie-list-by-genre/genre-resolver.serivce';
+import { MovieListByGenreResolver } from '@app/core/resolvers/movie-list-by-genre-resolver.service';
+import { GenreResolver } from '@app/core/resolvers/genre-resolver.serivce';
+import { MovieListByKeywordComponent } from '@app/movie/movie-list-by-keyword/movie-list-by-keyword.component';
+import { KeywordResolver, MovieListByKeywordResolver } from '@app/core';
 const routes: Routes = [
   {
     path: ':id',
@@ -26,6 +28,14 @@ const routes: Routes = [
     resolve : {
       genre: GenreResolver,
       movies: MovieListByGenreResolver
+    }
+  },
+  {
+    path: 'keywords/:id',
+    component: MovieListByKeywordComponent,
+    resolve : {
+      keyword: KeywordResolver,
+      movies: MovieListByKeywordResolver
     }
   },
   {
