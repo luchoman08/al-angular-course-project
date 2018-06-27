2a6c52df47058b5f0e27d0e59c714216a3a4dae0
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-27 13:26:48 -0500

    All specs deleted

--- src/app/shared/pipes/indefinite-article.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IndefiniteArticlePipe } from './indefinite-article.pipe';
-
-describe('IndefiniteArticlePipe', () => {
-  it('create an instance', () => {
-    const pipe = new IndefiniteArticlePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
