6df978e3cd6d97ee3d25829ebe049b3702c01ae6
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-27 13:33:36 -0500

    Add docstring to assets service

--- src/app/core/services/assets.service.ts
+++ src/app/core/services/assets.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
-
+/**
+ * Service that works as an interface to assets
+ *
+ * @class AssetsService
+ */
 @Injectable({
   providedIn: 'root'
 })
+
 export class AssetsService {
 
   constructor() { }
+  /**
+   * Return the url for default poster image from application assets
+   *
+   * @returns {string}
+   * @memberof AssetsService
+   */
   getDefaultPosterImageURL(): string {
     return './assets/img/posters/poster_default.png';
   }
+  /**
+   * Return the url for default profile image from application assets
+   *
+   * @returns {string}
+   * @memberof AssetsService
+   */
   getDefaltProfileImageURL(): string {
     return './assets/img/people/default-profile-men.png';
   }
