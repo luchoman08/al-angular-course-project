f0921d2c3abd7270c8caaec85471cda307f15679
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-06-27 13:29:35 -0500

    Image url now is using assets service

--- src/app/shared/pipes/imagesURL.pipe.ts
+++ src/app/shared/pipes/imagesURL.pipe.ts
@@ -5,26 +5,29 @@ import {
   ImageSizeValueModel,
   MediaTypeEnum
 } from '@app/core/models';
+import { AssetsService } from '@app/core';
 
 
 @Pipe({
   name: 'imageURL'
 })
 export class ImageURLPipe implements PipeTransform {
-    api_url  = environment.imagesTMDB_URL;
-    transform(path: string, size: ImageSizeValueModel, mediaType: MediaTypeEnum): string  {
-      if ( path && path != '' ) {
-        return `${this.api_url}/${size.name}${path}`;
-      } else if ( mediaType ) {
-        switch ( mediaType ) {
-          case MediaTypeEnum.MOVIE: {
-            return './assets/img/posters/poster_default.png';
-          }
-          case MediaTypeEnum.PERSON: {
-            return './assets/img/people/default-profile-men.png';
-          }
+  constructor(private assetsService: AssetsService) {
+
+  }
+  api_url = environment.imagesTMDB_URL;
+  transform(path: string, size: ImageSizeValueModel, mediaType: MediaTypeEnum): string {
+    if (path && path != '') {
+      return `${this.api_url}/${size.name}${path}`;
+    } else if (mediaType) {
+      switch (mediaType) {
+        case MediaTypeEnum.MOVIE: {
+          return this.assetsService.getDefaultPosterImageURL();
+        }
+        case MediaTypeEnum.PERSON: {
+          return this.assetsService.getDefaltProfileImageURL();
         }
       }
-
     }
   }
+}
