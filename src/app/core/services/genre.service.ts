9ce69c02ec2f6b3ac1191c7f0ede9c2e293b56aa
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-04 16:17:16 -0500

    Minor changes, genre service comments added

--- src/app/core/services/genre.service.ts
+++ src/app/core/services/genre.service.ts
@@ -9,24 +9,48 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class GenreService {
-  cacheGenres: Genre[];
+  /** Local cache for save genres */
+  private _cacheGenres: Genre[];
   constructor(
     private apiService: ApiService,
     private factoryService: FactoriesService) { 
       this.init_cache();
     }
+    /**
+     * Return genre from cache 
+     *
+     * @private
+     * @param {(string | number)} id
+     * @returns {Observable<Genre>}
+     * @memberof GenreService
+     */
     private getFromCache(id: string | number ): Observable<Genre> {
-      return of (this.cacheGenres.filter( genre => genre.id === Number(id) )[0]);
+      return of (this._cacheGenres.filter( genre => genre.id === Number(id) )[0]);
     }
+    /**
+     * Make a petition to TMDb and init the `_cache` property, is called at constructor
+     * of the `GenreService`
+     *
+     * @private
+     * @memberof GenreService
+     */
     private init_cache() {
       this.apiService.get('/genre/movie/list').subscribe(
         (results: {genres: GenreInterface[]}) => {
-          this.cacheGenres = this.factoryService.makeGenres(results.genres);
+          this._cacheGenres = this.factoryService.makeGenres(results.genres);
         }
       );
     }
+  /**
+   * Get genre, from cache if is inited, if the cache does not exist this is initialized and a query is made to API,
+   * next the value returnded by the API is returned as a `Genre` instance
+   *
+   * @param {(string | number)} id
+   * @returns {Observable<Genre>}
+   * @memberof GenreService
+   */
   get(id: string | number): Observable<Genre> {
-    if ( !this.cacheGenres ) {
+    if ( !this._cacheGenres ) {
       this.init_cache();
       return this.apiService.get('/genre/' + id);
     } else {
@@ -34,7 +58,15 @@ export class GenreService {
     }
     
   }
-
+  /**
+   * Get all the generes that correspond to the ids given. 
+   * If var `maxElements` is given, return only the first `maxElements` genres
+   *
+   * @param {number[]} ids
+   * @param {number} [maxElements]
+   * @returns {Observable<Genre[]>}
+   * @memberof GenreService
+   */
   getMultiple(ids: number[], maxElements?: number): Observable<Genre[]>{
     maxElements? null : maxElements = ids.length  ; 
     return forkJoin(ids.slice(0, maxElements).map(
