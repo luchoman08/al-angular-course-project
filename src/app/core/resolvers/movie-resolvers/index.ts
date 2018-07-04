27190aee03ac4afd6158dd7d8568627d8cae54e2
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-04 16:14:32 -0500

    Movie resolvers moved to core/resolvers/movie

---
+++ src/app/core/resolvers/movie-resolvers/index.ts
@@ -0,0 +1,5 @@
+export * from './movie-list-by-genre-resolver.service';
+export * from './movie-list-by-keyword-resolver.service';
+export * from './movie-detail-resolver.service';
+export * from './movie-full-credits-resolver.service';
+export * from './movie-search-resolver.serivce'
