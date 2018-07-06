a67a29a90fd3a16aa00d89eb969d7841e5cfe288
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-05 21:20:10 -0500

    Default size added to poster image directive, for fix default poster image size when default poster image is small than poster image

--- src/app/shared/directives/poster-image.directive.ts
+++ src/app/shared/directives/poster-image.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, ElementRef,  OnChanges } from '@angular/core';
+import { Directive, Input, ElementRef, OnChanges, Renderer2 } from '@angular/core';
 import { AssetsService, ImageSizeValueModel, MediaTypeEnum } from '@app/core';
 import { ImageURLPipe } from '@app/shared/pipes';
 
@@ -8,20 +8,25 @@ import { ImageURLPipe } from '@app/shared/pipes';
 export class PosterImageDirective implements OnChanges {
   @Input() path: string;
   @Input() size: ImageSizeValueModel;
-  constructor( 
+  /** Keep default size defined by `size` Input argument or not */
+  @Input() default_size?: boolean;
+  constructor(
     private el: ElementRef,
+    private renderer: Renderer2,
     private assetsService: AssetsService,
-    private imageURLPipe: ImageURLPipe) { 
+    private imageURLPipe: ImageURLPipe) {
 
-   }
-   ngOnChanges() {
-    this.el.nativeElement.width = this.size.width;
-
-    if (this.path) {
-      this.el.nativeElement.src = this.imageURLPipe.transform(this.path, this.size, MediaTypeEnum.MOVIE);
-    } else {
-      this.el.nativeElement.src = this.assetsService.getDefaultPosterImageURL();
+  }
+  ngOnChanges() {
+    if ( this.default_size ) {
+      this.renderer.setStyle(this.el.nativeElement, 'min-width', this.size.width + 'px');
+      this.renderer.setStyle(this.el.nativeElement, 'min-height', this.size.height + 'px');
     }
-   }
+    if (this.path) {
+      this.renderer.setProperty(this.el.nativeElement, 'src', this.imageURLPipe.transform(this.path, this.size, MediaTypeEnum.MOVIE));
+    } else {
+      this.renderer.setProperty(this.el.nativeElement, 'src', this.assetsService.getDefaultPosterImageURL());
+    }
+  }
 
 }
