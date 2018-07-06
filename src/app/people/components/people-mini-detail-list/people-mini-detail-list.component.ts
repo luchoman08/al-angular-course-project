f12ec54966171b8e13e20a2af1a0faa474736241
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-07-05 21:09:21 -0500

    People mini detail list, emit when the scroll touch the end

--- src/app/people/components/people-mini-detail-list/people-mini-detail-list.component.ts
+++ src/app/people/components/people-mini-detail-list/people-mini-detail-list.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Person } from '@app/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { ScrollEvent } from 'ngx-scroll-event';
 @Component({
   selector: 'app-people-mini-detail-list',
   templateUrl: './people-mini-detail-list.component.html',
   styleUrls: ['./people-mini-detail-list.component.scss']
 })
 export class PeopleMiniDetailListComponent implements OnInit {
-
-  constructor() { }
+  @Input() people: Person[];
+  @Output() endList = new EventEmitter<boolean>();
+  private debouncer: Subject<boolean> = new Subject<boolean>();
+  constructor() {
+        /** Emit each 1000 milliseconds event `end of list` */
+        this.debouncer.pipe(
+          debounceTime(1000))
+          .subscribe((val) => this.endList.emit(val));
+   }
 
   ngOnInit() {
+  }
+
+    /**
+   * Emit `true` when the user has been see all movie details
+   *
+   * @memberof MovieMiniDetailListComponent
+   */
+  emitEnd() {
+    this.debouncer.next(true);
+  }
+  /**
+   * Handle scroll events and react when the user is at end of component
+   *
+   * @param {ScrollEvent} event
+   * @memberof MovieMiniDetailListComponent
+   */
+  public handleScroll(event: ScrollEvent) {
+    if (event.isReachingBottom) {
+      this.emitEnd();
+    }
   }
 
 }
