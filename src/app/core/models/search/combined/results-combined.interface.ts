64ef91fe2d7485859d7c7845b5cafe15a10a6c6c
Luis Gerardo Manrique Cardona <lmanrique@truora.com>
2023-12-19 22:10:28 -0500

    remove unused files

--- src/app/core/models/search/combined/results-combined.interface.ts
+++
@@ -1,7 +0,0 @@
-import { ResultsInterface } from '@app/core/models/interfaces/results.interface';
-import { TvCombinedInterface } from '@app/core/models/search/combined/tv-combined.interface';
-import { PersonCombinedInterface } from '@app/core/models/search/combined/person-combined.interface';
-import { MovieCombinedInterface } from '@app/core/models/search/combined/movie-combined.interface';
-export interface ResultsCombined extends ResultsInterface<TvCombinedInterface | PersonCombinedInterface | MovieCombinedInterface> {
-
-}
